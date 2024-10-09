d442444bfe80c63b05e615aeaa2144364a52b4f4
DaniCabrera91 <danielcabreravazquez91@gmail.com>
2024-10-09 18:42:40 +0200

    modified User model schema with new fields

--- models/User.js
+++ models/User.js
@@ -1,4 +1,23 @@
 const mongoose = require('mongoose')
-const UserSchema = new mongoose.Schema()
-const User = mongoose.model('User', UserSchema)
-MediaSourceHandle.esports = User
+const Schema = mongoose.Schema
+
+// Definir el esquema del usuario
+const userSchema = new Schema(
+  {
+    profile: { type: String, required: true }, // Ej: buyer_1
+    name: { type: String, required: true }, // Nombre del usuario
+    surname: { type: String, required: true }, // Apellido del usuario
+    birth_date: { type: Date, required: true }, // Fecha de nacimiento
+    dni: { type: String, required: true, unique: true }, // DNI único
+    email: { type: String, required: true, unique: true }, // Email único
+    iban: { type: String, required: true, unique: true }, // IBAN único
+    balance: { type: Number, required: true }, // Saldo del usuario
+  },
+  {
+    timestamps: true, // Añade createdAt y updatedAt automáticamente
+  },
+)
+
+// Crear el modelo del usuario
+const User = mongoose.model('User', userSchema)
+module.exports = User
