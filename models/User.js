ce5fcd8f1b76f523782c746ee727db9bf38c5dad
DaniCabrera91 <danielcabreravazquez91@gmail.com>
2024-10-09 20:06:01 +0200

    modified user model with some validations and instaled bcrypt

--- models/User.js
+++ models/User.js
@@ -1,23 +1,57 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-// Definir el esquema del usuario
 const userSchema = new Schema(
   {
-    profile: { type: String, required: true }, // Ej: buyer_1
-    name: { type: String, required: true }, // Nombre del usuario
-    surname: { type: String, required: true }, // Apellido del usuario
-    birth_date: { type: Date, required: true }, // Fecha de nacimiento
-    dni: { type: String, required: true, unique: true }, // DNI único
-    email: { type: String, required: true, unique: true }, // Email único
-    iban: { type: String, required: true, unique: true }, // IBAN único
-    balance: { type: Number, required: true }, // Saldo del usuario
+    profile: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    surname: {
+      type: String,
+      required: true,
+    },
+    birth_date: {
+      type: Date,
+      required: true,
+    },
+    dni: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    iban: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    balance: {
+      type: Number,
+      required: true,
+    },
   },
   {
-    timestamps: true, // Añade createdAt y updatedAt automáticamente
+    timestamps: true,
   },
 )
 
-// Crear el modelo del usuario
 const User = mongoose.model('User', userSchema)
 module.exports = User
